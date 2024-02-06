8a0e1670810fb7d9dacf004c515be8022c6e513d
AMIT KUMAR <88123144+amit-kumar81@users.noreply.github.com>
2024-02-06 14:14:35 +0530

    Create main.py

---
+++ Day-1/Module-&-Pip/main.py
@@ -0,0 +1,9 @@
+import pandas  # This is an example of external module
+import hashlib  # This is an example of built in module
+
+print("Hi")
+
+# use of external module
+pandas.read_csv("one.csv")
+# use of built-in-module
+m = hashlib.sha256()
