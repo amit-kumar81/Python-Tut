029dd89de5ef7e9e7285a91ce5c92c9a31ce31c0
AMIT KUMAR <88123144+amit-kumar81@users.noreply.github.com>
2024-02-08 17:36:38 +0530

    Update main.py

--- Day-3/Dictionaries/main.py
+++ Day-3/Dictionaries/main.py
@@ -9,4 +9,13 @@ info = {'name':'Karan', 'age':19, 'eligible':True}
 print(info.items())
 for key, value in info.items():
   print(f"The value corresponding to the key {key} is {value}") 
-  
+#-------------------------------------------------------------------------------------------------------------------------------------------
+ep1 = {122: 45, 123: 89, 567: 69, 670: 69}
+ep2 = {222: 67, 566: 90}
+
+# ep1.update(ep2)
+# ep1.clear()
+# ep1.pop(122)
+ep1.popitem()
+del ep1[122]
+print(ep1) 
